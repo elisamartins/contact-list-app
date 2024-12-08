f62408cf11c14db852231a2628e3951b8daf11e4
Elisa <elisa_cm@msn.com>
2024-12-07 21:23:35 -0500

    Added form submission handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Grid2 } from "@mui/material";
 import ContactList from "./components/ContactList";
-import { cacheKey, getContacts } from "./api/api";
+import { cacheKey, createContact, getContacts, updateContact } from "./api/api";
 import useSWR from "swr";
 import { useState } from "react";
 import ContactDetails from "./components/ContactDetails";
-import { IContact } from "./types";
+import { IContact, IContactFormData } from "./types";
+import ContactFormDialog from "./components/ContactFormDialog";
+import {
+	createContactOptions,
+	updateContactOptions,
+} from "./api/mutateOptions";
 
 function App() {
+	const [formDialogOpen, setFormDialogOpen] = useState<boolean>(false);
 	const [selectedContactId, setSelectedContactId] = useState<string | null>(
 		null
 	);
@@ -25,13 +31,38 @@ function App() {
 		(contact: IContact) => contact.id == selectedContactId
 	);
 
+	function handleFormSubmit(formData: IContactFormData) {
+		if (selectedContactId) {
+			mutate(
+				updateContact(selectedContactId, formData),
+				updateContactOptions(selectedContactId, formData)
+			);
+		} else {
+			mutate(createContact(formData), createContactOptions(formData));
+		}
+    setFormDialogOpen(false);
+	}
+
 	return (
 		<Grid2 container>
 			<ContactList
 				contacts={contacts}
 				onClickContact={(id) => setSelectedContactId(id)}
+				onClickAddContact={() => setFormDialogOpen(true)}
 			/>
-			{selectedContact && <ContactDetails contact={selectedContact} />}
+			{selectedContact && (
+				<ContactDetails
+					key={`${selectedContactId}-contact-details`}
+					contact={selectedContact}
+				/>
+			)}
+
+			<ContactFormDialog
+				key={`${selectedContactId || "create-contact"}-form-dialog`}
+				open={formDialogOpen}
+				onClickCancel={() => setFormDialogOpen(false)}
+				onSubmitForm={handleFormSubmit}
+			/>
 		</Grid2>
 	);
 }
