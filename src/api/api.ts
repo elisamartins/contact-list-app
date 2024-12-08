6df3f74a08732e232d7a3e5051714e7d06298b71
Elisa <elisa_cm@msn.com>
2024-12-08 11:43:08 -0500

    Added config to deploy json-server

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,14 @@
 import { IContactFormData } from "../types";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 export const cacheKey = "/contacts";
 
 export function getContacts() {
-	return fetch(`${BASE_URL}${cacheKey}`).then((result) => result.json());
+	return fetch(`${API_URL}${cacheKey}`).then((result) => result.json());
 }
 
 export function createContact(contactFormData: IContactFormData) {
-	return fetch(`${BASE_URL}${cacheKey}`, {
+	return fetch(`${API_URL}${cacheKey}`, {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify(contactFormData),
@@ -19,7 +19,7 @@ export function updateContact(
 	contactId: string,
 	contactFormData: IContactFormData
 ) {
-	return fetch(`${BASE_URL}${cacheKey}/${contactId}`, {
+	return fetch(`${API_URL}${cacheKey}/${contactId}`, {
 		method: "PUT",
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify(contactFormData),
@@ -27,7 +27,7 @@ export function updateContact(
 }
 
 export function deleteContact(contactId: string) {
-	return fetch(`${BASE_URL}${cacheKey}/${contactId}`, {
+	return fetch(`${API_URL}${cacheKey}/${contactId}`, {
 		method: "DELETE",
 		headers: { "Content-Type": "application/json" },
 	}).then((result) => result.json());
